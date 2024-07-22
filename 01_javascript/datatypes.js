1a96ce24505b1519bb1ba58a6c11fdece0d2f657
Namami-singh <114169399+Namami-singh@users.noreply.github.com>
2024-07-22 19:01:00 +0000

    datatypes

---
+++ 01_javascript/datatypes.js
@@ -0,0 +1,18 @@
+"use strict";//tell the engine to treat all the code with newer version standard
+
+/*alert("mesaage"); because broswer mein run krega ye method 
+alert(1+1)        and we are using nodejs(js working engine) not browser */
+
+//datatypes -number(2^53),bigint(for extra large values),string,boolean
+//null(standalone value and is of object type) , undefined ,symbol(to depict uniqueness)
+//object
+console.log(typeof 12345678)
+console.log(typeof "null")
+console.log(typeof true)
+console.log(typeof 578656487567565345635637565)
+console.log(typeof undefined)
+console.log(typeof null)
+
+let temp=(2**53)+1
+console.log(temp)
+console.log(typeof temp)

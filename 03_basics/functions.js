58e505a77ce44dcf41dca01fcd16bfd1f1e8ef2c
Namami-singh <114169399+Namami-singh@users.noreply.github.com>
2024-08-28 20:33:52 +0000

    somechanges

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -23,5 +23,34 @@ function loginusermsg(username= "User"){
 }
 
 // let result1=loginusermsg("")
-let result1=loginusermsg("Elon musk")
-console.log(result1);
+// let result1=loginusermsg("Elon musk")
+
+// console.log(result1);
+
+
+
+function countCartPrice(...num1){//rest operator
+    return num1
+}
+// const cartPrice=countCartPrice(1000,300,200,500)
+// console.log(cartPrice)
+
+const User={
+    Id: 10116765732889,
+    subscription: "active"
+}
+
+function UserSubDefinition(mainobj){
+    console.log(`The ID number ${mainobj.Id} is ${mainobj.subscription}`);
+}
+
+// UserSubDefinition(User)
+UserSubDefinition({Id: 2011676542889,
+    subscription: "Deactive"})
+
+
+const arr=[10,60,4050,87465]
+function getElementone(anyarray){
+    return anyarray[1]
+}
+console.log(getElementone([1,2,3,4]))

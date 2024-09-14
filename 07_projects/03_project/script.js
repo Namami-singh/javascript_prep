2c8f0754a097e7a7093be74434b46c65e84a54fe
Namami-singh <114169399+Namami-singh@users.noreply.github.com>
2024-09-14 18:59:53 +0000

    some more changes-proj3

--- 07_projects/03_project/script.js
+++ 07_projects/03_project/script.js
@@ -1,7 +1,17 @@
 let timeblock=document.querySelector(".time");
-
+const alarm=document.querySelector(".alarm");
+const alarm_set_popup=document.querySelector(".alarm-popup");
+const form=document.querySelector("form");
+const time_slot=document.querySelector("#date-time");
 
 setInterval(() => {
     let time=new Date();
     timeblock.innerHTML = time.toLocaleTimeString();
 }, 1000);
+
+
+alarm.addEventListener("click",()=>{
+    alarm_set_popup.style.display="inline-block";
+    alarm_set_popup.style.top="-35vh";
+    alarm_set_popup.style.left="35vw";
+})

1aa7495212d19b3b3b1bdbe1c85bc926cccac828
Namami-singh <114169399+Namami-singh@users.noreply.github.com>
2024-11-07 18:43:08 +0000

    promise(more)

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -58,3 +58,54 @@ function cleanup(msg) {
     console.log("finally" + msg);
     
 }
+
+
+//
+const promisenew= new Promise((resolve,reject)=>{
+    let error=false;
+    let obj={username:"Lmfao" , password: "kuch to hai"}
+    if(!error)resolve(obj);
+    else reject("error");
+})
+//js priortize then in its event loop over any finally present in there
+promisenew.then((user)=>{
+    console.log(`The beautiful name of the user is ${user["username"]}`)
+    return user.password;
+})
+.then((password)=>{
+    console.log(`The long thought password is: ${password}`);
+});
+
+
+//
+const promise_async =new Promise((resolve,reject)=>//this section is interesting run it and try it 
+{
+    setTimeout(()=>{
+        setTimeout(()=>{
+            resolve("Inner timeout done");
+        },1000);
+        reject("outer Timeout");// this outputs as inner function does not execute until outer is done and when it starts we have to 
+                                //wait for one sec so it is in waitng block till then execution continues and "outer Timeout"gets printed
+    },2000);
+})
+
+async function promise_try(){
+
+   try{
+    let result= await promise_async;
+    console.log(result);
+   }
+   catch(error)
+   {
+    console.log(error);
+    
+   }
+}
+promise_try();
+
+/*
+setTimeout is asynchronous, meaning it schedules a function to run in the future but doesn't wait for it to complete.
+The try...catch block executes synchronously, so it finishes before the setTimeout callback is called.
+ */
+
+fetch("www.youtube.com")
